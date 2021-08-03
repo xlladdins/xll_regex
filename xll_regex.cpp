4ec614633224aa8a4925b38f4665bdfb25d59559
Keith A. Lewis <keithalewis@live.com>
2021-08-03 06:41:03 -0400

    documentation

--- xll_regex.cpp
+++ xll_regex.cpp
@@ -10,11 +10,17 @@ using xcstr = traits<XLOPERX>::xcstr;
 AddIn xai_regex_search(
 	Function(XLL_LPOPER, "xll_regex_search", "REGEX.SEARCH")
 	.Arguments({
-		Arg(XLL_CSTRING, "re", "is a regular expression."),
+		Arg(XLL_CSTRING, "regex", "is a regular expression."),
 		Arg(XLL_CSTRING, "string", "is a string."),
 		})
 	.Category("Regex")
-	.FunctionHelp("")
+	.FunctionHelp("Search for a regular expression match.")
+	.HelpTopic("https://docs.microsoft.com/en-us/cpp/standard-library/regex-functions#regex_search")
+	.Documentation(R"(
+Search <code>string</code> for matching results of
+regular expression <code>regex</code>. Return <code>FALSE</code>
+if there is no match or a one column array of all sub matches.
+)")
 );
 LPOPER WINAPI xll_regex_search(xcstr regex, xcstr str)
 {
@@ -22,7 +28,7 @@ LPOPER WINAPI xll_regex_search(xcstr regex, xcstr str)
 	static OPER o;
 
 	try {
-		o = ErrNA;
+		o = false;
 
 		std::match_results<xcstr> mr;
 		std::basic_regex<xchar> re(regex);
@@ -36,6 +42,8 @@ LPOPER WINAPI xll_regex_search(xcstr regex, xcstr str)
 	}
 	catch (const std::exception& ex) {
 		XLL_ERROR(ex.what());
+
+		o = ErrNA;
 	}
 
 	return &o;
@@ -45,6 +53,7 @@ LPOPER WINAPI xll_regex_search(xcstr regex, xcstr str)
 
 #define XLL_REGEX_TEST(X) \
 	X(_T(".*"), _T("abc"), _T("abc")) \
+	X(_T(".(b)."), _T("abc"), _T("abc"), _T("b")) \
 
 int xll_regex_test()
 {
