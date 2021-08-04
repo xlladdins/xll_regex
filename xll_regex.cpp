367983e772620d11f8906ada22c591bf294645cf
Keith A. Lewis <keithalewis@live.com>
2021-08-04 09:47:43 -0400

    regex and search flags

--- xll_regex.cpp
+++ xll_regex.cpp
@@ -7,22 +7,32 @@ using namespace xll;
 using xchar = traits<XLOPERX>::xchar;
 using xcstr = traits<XLOPERX>::xcstr;
 
+#define REGEX_FLAG_TYPE(a,b,c) XLL_CONST(LONG, REGEX_SYNTAX_ ## a, std::regex_constants:: ## b, c, CATEGORY, REGEX_CONSTANTS_TOPIC)
+REGEX_CONSTANTS_SYNTAX(REGEX_FLAG_TYPE)
+#undef REGEX_FLAG_TYPE
+
+#define REGEX_MATCH_TYPE(a,b,c) XLL_CONST(LONG, REGEX_MATCH_ ## a, std::regex_constants:: ## b, c, CATEGORY, REGEX_CONSTANTS_TOPIC)
+REGEX_CONSTANTS_MATCH(REGEX_MATCH_TYPE)
+#undef REGEX_MATCH_TYPE
+
 AddIn xai_regex_(
 	Function(XLL_HANDLEX, "xll_regex_", "\\REGEX")
 	.Arguments({
 		Arg(XLL_CSTRING, "regex", "is a regular expression."),
+		Arg(XLL_LONG, "flags", "are optional flags from REGEX_FLAG_*.")
 		})
 		.Uncalced()
+	.Category(CATEGORY)
 	.FunctionHelp("Return a handle to a compiled regular expression.")
 	.HelpTopic("https://docs.microsoft.com/en-us/cpp/standard-library/basic-regex-class")
 	.Documentation(R"xyzyx()xyzyx")
 );
-HANDLEX WINAPI xll_regex_(xcstr re)
+HANDLEX WINAPI xll_regex_(xcstr re, std::regex_constants::syntax_option_type flags)
 {
 #pragma XLLEXPORT
 	HANDLEX h = INVALID_HANDLEX;
 
-	handle<std::basic_regex<xchar>> re_(new std::basic_regex<xchar>(re));
+	handle<std::basic_regex<xchar>> re_(new std::basic_regex<xchar>(re, flags));
 	if (re_) {
 		h = re_.get();
 	}
@@ -35,6 +45,7 @@ AddIn xai_regex_search(
 	.Arguments({
 		Arg(XLL_LPOPER, "regex", "is a regular expression or handle."),
 		Arg(XLL_CSTRING, "string", "is a string."),
+		Arg(XLL_LONG, "flags", "are optional flags from REGEX_MATCH_*")
 		})
 	.Category("Regex")
 	.FunctionHelp("Search for a regular expression match.")
@@ -45,7 +56,7 @@ regular expression <code>regex</code>. Return <code>FALSE</code>
 if there is no match or a one column array of all sub matches.
 )")
 );
-LPOPER WINAPI xll_regex_search(const LPOPER pre, xcstr str)
+LPOPER WINAPI xll_regex_search(const LPOPER pre, xcstr str, std::regex_constants::match_flag_type flags)
 {
 #pragma XLLEXPORT
 	static OPER o;
@@ -57,12 +68,12 @@ LPOPER WINAPI xll_regex_search(const LPOPER pre, xcstr str)
 		if (pre->is_num()) {
 			handle<std::basic_regex<xchar>> re_(pre->as_num());
 			if (re_) {
-				o = std::regex_search(str, mr, *re_);
+				o = std::regex_search(str, mr, *re_, flags);
 			}
 		}
 		else if (pre->is_str()) {
 			std::basic_regex<xchar> re(pre->val.str + 1, pre->val.str[0]);
-			o = std::regex_search(str, mr, re);
+			o = std::regex_search(str, mr, re, flags);
 		}
 		else {
 			o = ErrValue;
@@ -94,7 +105,7 @@ LPOPER WINAPI xll_regex_search(const LPOPER pre, xcstr str)
 int xll_regex_test()
 {
 	try {
-#define RE_TEST(a, b, ...) { OPER _a(a); ensure(*xll_regex_search(&_a, b) == OPER::make(__VA_ARGS__)); }
+#define RE_TEST(a, b, ...) { OPER _a(a); ensure(*xll_regex_search(&_a, b, std::regex_constants::match_default) == OPER::make(__VA_ARGS__)); }
 		XLL_REGEX_TEST(RE_TEST)
 #undef RE_TEST
 	}
--- xll_regex.h
+++ xll_regex.h
@@ -2,3 +2,38 @@
 #pragma once
 
 #include "xll/xll/xll.h"
+
+#ifndef CATEGORY
+#define CATEGORY "Regex"
+#endif
+
+#define REGEX_CONSTANTS_TOPIC "https://docs.microsoft.com/en-us/cpp/standard-library/regex-constants-class"
+
+#define REGEX_CONSTANTS_SYNTAX(X) \
+X(AWK, awk, "compile as awk") \
+X(BASIC, basic, "compile as BRE") \
+X(ECMASCRIPT, ECMAScript, "compile as ECMAScript") \
+X(EGREP, egrep, "compile as egrep") \
+X(EXTENDED, extended, "compile as ERE") \
+X(GREP, grep, "compile as grep") \
+X(ICASE, icase, "make matches case") \
+X(NOSUBS, nosubs, "the implementaton need not keep track of the contents of capture groups") \
+X(OPTIMIZE, optimize, "the implementation should emphasize speed of matching rather than speed of regular expression compilation") \
+
+#define REGEX_CONSTANTS_MATCH(X) \
+X(FORMAT_DEFAULT, format_default, "use ECMAScript format rules") \
+X(FORMAT_FIRST_ONLY, format_first_only, "do not search for matches after the first one") \
+X(FORMAT_NO_COPY, format_no_copy, "do not copy text that does not match the regular expression") \
+X(FORMAT_SED, format_sed, "use sed format rules") \
+X(MATCH_ANY, match_any, "if more than one match is possible any match is acceptable") \
+X(MATCH_CONTINUOUS, match_continuous, "do not search for matches other than at the beginning of the target sequence") \
+X(MATCH_DEFAULT, match_default, " ") \
+X(MATCH_NOT_BOL, match_not_bol, "do not treat the first position in the target sequence as the beginning of a line") \
+X(MATCH_NOT_BOW, match_not_bow, "do not treat the first position in the target sequence as the beginning of a word") \
+X(MATCH_NOT_EOL, match_not_eol, "do not treat the past the end position in the target sequence as the end of a line") \
+X(MATCH_NOT_EOW, match_not_eow, "do not treat the past the end position in the target sequence as the end of a word") \
+X(MATCH_NOT_NULL, match_not_null, "do not treat an empty subsequence as a match") \
+X(MATCH_PREV_AVAIL, match_prev_avail, "first is a valid iterator; ignore match_not_bol and match_not_bow if set") \
+
+
+// REGEX_CONSTANTS_ERROR
